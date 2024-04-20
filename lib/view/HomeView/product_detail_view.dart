c356c5c862ee1a0a0af7d2f5b34622e8d6bae025
Basit ali <basitalyshah51214@gmail.com>
2024-04-20 00:01:25 -0700

    changes to add icon

--- lib/view/HomeView/product_detail_view.dart
+++ lib/view/HomeView/product_detail_view.dart
@@ -37,7 +37,7 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
                       Navigator.pop(context);
                     },
                     icon: const Icon(
-                      Icons.arrow_back,
+                      Icons.west,
                       color: AppColor.textColor1,
                     ),
                   ),
