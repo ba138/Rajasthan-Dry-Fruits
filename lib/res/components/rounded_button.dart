38d6e7ad29f26b34aa08c4109a673efe4f0b871b
Basit ali <basitalyshah51214@gmail.com>
2024-04-20 03:41:28 -0700

    changes to develop check out screen

--- lib/res/components/rounded_button.dart
+++ lib/res/components/rounded_button.dart
@@ -19,9 +19,17 @@ class RoundedButton extends StatelessWidget {
         height: 56,
         width: MediaQuery.of(context).size.width,
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          color: AppColor.primaryColor,
-        ),
+            borderRadius: BorderRadius.circular(10.0),
+            color: AppColor.primaryColor,
+            boxShadow: [
+              BoxShadow(
+                color: const Color.fromARGB(255, 0, 0, 0)
+                    .withOpacity(0.25), // Shadow color
+                blurRadius: 8.1, // Blur radius
+                spreadRadius: 0, // Spread radius
+                offset: const Offset(0, 4), // Offset
+              ),
+            ]),
         child: Center(
           child: Text(
             title,
---
+++ lib/view/checkOut/widgets/payment_field.dart
@@ -0,0 +1,98 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:rjfruits/res/components/colors.dart';
+
+class PaymentField extends StatefulWidget {
+  const PaymentField({
+    super.key,
+    this.hintText,
+    required int maxLines,
+    required this.text,
+    this.controller,
+    this.keyboardType,
+    this.obscureText = false,
+    this.validator,
+  });
+
+  final String text;
+  final TextEditingController? controller;
+  final TextInputType? keyboardType;
+  final bool obscureText;
+  final String? hintText;
+  final String? Function(String?)? validator;
+  @override
+  State<PaymentField> createState() => _PaymentFieldState();
+}
+
+class _PaymentFieldState extends State<PaymentField> {
+  bool hidden = true;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            widget.text,
+            style: GoogleFonts.getFont(
+              "Poppins",
+              textStyle: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                color: AppColor.dashboardIconColor,
+              ),
+            ),
+          ),
+          const SizedBox(height: 8),
+          Container(
+            color: const Color(0xffEEEEEE),
+            child: TextFormField(
+              keyboardType: widget.keyboardType,
+              obscureText: (widget.obscureText && hidden),
+              style: const TextStyle(fontSize: 15),
+              controller: widget.controller,
+              decoration: InputDecoration(
+                hintStyle: GoogleFonts.getFont(
+                  "Roboto",
+                  textStyle: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: AppColor.textColor1,
+                  ),
+                ),
+                // prefixText:  widget.hintText,
+                hintText: widget.hintText,
+                filled: true,
+                suffixIcon: widget.obscureText
+                    ? GestureDetector(
+                        onTap: () {
+                          setState(() => hidden = !hidden);
+                        },
+                        child: Icon(
+                          hidden ? Icons.visibility_off : Icons.visibility,
+                          color: hidden ? null : AppColor.primaryColor,
+                          size: 30,
+                        ),
+                      )
+                    : null,
+                fillColor: const Color(0xffEEEEEE),
+                enabledBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xfff1f1f1)),
+                  borderRadius: BorderRadius.zero,
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xfff1f1f1)),
+                  borderRadius: BorderRadius.zero,
+                ),
+              ),
+              validator: widget.validator,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
