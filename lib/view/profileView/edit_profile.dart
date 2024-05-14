bb8f28bad2e1b5016e3fcaaa78f367884618a75a
Basit ali <basitalyshah51214@gmail.com>
2024-05-14 00:16:28 -0700

    done edit profile screen

--- lib/view/profileView/edit_profile.dart
+++ lib/view/profileView/edit_profile.dart
@@ -27,6 +27,19 @@ class _EditProfileState extends State<EditProfile> {
   TextEditingController firstNameController = TextEditingController();
   TextEditingController lastNameController = TextEditingController();
   TextEditingController emailController = TextEditingController();
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    firstNameController = TextEditingController(
+      text: widget.firstName,
+    );
+    lastNameController = TextEditingController(
+      text: widget.lastName,
+    );
+    emailController = TextEditingController(
+      text: widget.email,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,100 +54,106 @@ class _EditProfileState extends State<EditProfile> {
         ),
         child: Padding(
           padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-          child: Column(
-            children: [
-              const VerticalSpeacing(30.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: const Icon(
-                      Icons.west,
-                      color: AppColor.appBarTxColor,
-                    ),
-                  ),
-                  Text(
-                    'Pofile',
-                    style: GoogleFonts.getFont(
-                      "Poppins",
-                      textStyle: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w500,
+          child: SingleChildScrollView(
+            physics: const NeverScrollableScrollPhysics(),
+            child: Column(
+              children: [
+                const VerticalSpeacing(30.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(
+                        Icons.west,
                         color: AppColor.appBarTxColor,
                       ),
                     ),
-                  ),
-                  Text(
-                    'Edit Profile',
-                    style: GoogleFonts.getFont(
-                      "Poppins",
-                      textStyle: const TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.transparent,
+                    Text(
+                      'Pofile',
+                      style: GoogleFonts.getFont(
+                        "Poppins",
+                        textStyle: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: AppColor.appBarTxColor,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              const VerticalSpeacing(30.0),
-              Container(
-                height: MediaQuery.of(context).size.height / 1.2,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(color: AppColor.primaryColor, width: 1),
-                  color: const Color.fromRGBO(
-                      255, 255, 255, 0.2), // Background color with opacity
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.white.withOpacity(0.5), // Shadow color
-                      blurRadius: 2, // Blur radius
-                      spreadRadius: 0, // Spread radius
-                      offset: const Offset(0, 0), // Offset
+                    Text(
+                      'Edit Profile',
+                      style: GoogleFonts.getFont(
+                        "Poppins",
+                        textStyle: const TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.transparent,
+                        ),
+                      ),
                     ),
                   ],
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: ListView(
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const PaymentField(
-                            maxLines: 2,
-                            text: 'First Name',
-                            hintText: 'Hiren user',
-                          ),
-                          const PaymentField(
-                            maxLines: 2,
-                            text: 'Last Name',
-                            hintText: 'user',
-                          ),
-                          const PaymentField(
-                            maxLines: 2,
-                            text: 'Email',
-                            hintText: 'Example@gmail.com',
-                          ),
-                          const VerticalSpeacing(60.0),
-                          RoundedButton(
-                              title: 'Save',
-                              onpress: () {
-                                Navigator.pop(context);
-                              }),
-                          const VerticalSpeacing(20.0),
-                        ],
+                const VerticalSpeacing(30.0),
+                Container(
+                  height: MediaQuery.of(context).size.height / 1.2,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: AppColor.primaryColor, width: 1),
+                    color: const Color.fromRGBO(
+                        255, 255, 255, 0.2), // Background color with opacity
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.white.withOpacity(0.5), // Shadow color
+                        blurRadius: 2, // Blur radius
+                        spreadRadius: 0, // Spread radius
+                        offset: const Offset(0, 0), // Offset
                       ),
                     ],
                   ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: ListView(
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            PaymentField(
+                              controller: firstNameController,
+                              maxLines: 2,
+                              text: 'First Name',
+                              hintText: 'Hiren user',
+                            ),
+                            PaymentField(
+                              controller: lastNameController,
+                              maxLines: 2,
+                              text: 'Last Name',
+                              hintText: 'user',
+                            ),
+                            PaymentField(
+                              controller: emailController,
+                              maxLines: 2,
+                              text: 'Email',
+                              hintText: 'Example@gmail.com',
+                            ),
+                            const VerticalSpeacing(60.0),
+                            RoundedButton(
+                                title: 'Save',
+                                onpress: () {
+                                  Navigator.pop(context);
+                                }),
+                            const VerticalSpeacing(20.0),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
