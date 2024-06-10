d6f4eda9451e1dd74d83aef155adab93da79aa43
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2024-06-10 19:16:41 +0500

    copons done

--- lib/view/profileView/promo_view.dart
+++ lib/view/profileView/promo_view.dart
@@ -1,9 +1,14 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
 import 'package:rjfruits/res/components/colors.dart';
 import 'package:rjfruits/utils/routes/utils.dart';
 import 'package:rjfruits/view/profileView/widgets/promo_card.dart';
+
+import '../../view_model/user_view_model.dart';
 
 class PromoView extends StatefulWidget {
   const PromoView({super.key});
@@ -13,6 +18,44 @@ class PromoView extends StatefulWidget {
 }
 
 class _PromoViewState extends State<PromoView> {
+  List<Map<String, String>> _coupons = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchCoupons();
+  }
+
+  Future<void> _fetchCoupons() async {
+    final userPreferences = Provider.of<UserViewModel>(context, listen: false);
+    final userModel = await userPreferences.getUser();
+    final token = userModel.key;
+
+    final response = await http.get(
+      Uri.parse('http://103.117.180.187/api/coupon-list/'),
+      headers: {
+        'accept': 'application/json',
+        'X-CSRFToken':
+            'EPfbegDrcoMcKsg8rk5bx1bMotQv2GCk5hvuKZYUbALSqcufI1DK4ZIbzkRUnWg2',
+        'authorization': 'Token $token',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      setState(() {
+        _coupons = data.map((coupon) {
+          return {
+            'discount': coupon['discount'] as String,
+            'code': coupon['code'] as String,
+          };
+        }).toList();
+      });
+    } else {
+      // Handle error
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,28 +93,33 @@ class _PromoViewState extends State<PromoView> {
         ),
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: GridView.count(
-            padding: const EdgeInsets.all(5.0),
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            crossAxisCount: 2,
-            childAspectRatio: (230 / 255),
-            mainAxisSpacing: 10.0,
-            crossAxisSpacing: 10.0,
-            children: List.generate(
-              // Limit to only two items
-              2,
-              (index) => PromoCard(
-                discount: "20",
-                onpress: () {
-                  Clipboard.setData(
-                    const ClipboardData(text: "promocode"),
-                  );
-                  Utils.toastMessage("Code copied to clipboard");
-                },
-              ),
-            ),
-          ),
+          child: _coupons.isEmpty
+              ? const Center(child: CircularProgressIndicator())
+              : GridView.count(
+                  padding: const EdgeInsets.all(5.0),
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  crossAxisCount: 2,
+                  childAspectRatio: (230 / 255),
+                  mainAxisSpacing: 10.0,
+                  crossAxisSpacing: 10.0,
+                  children: List.generate(
+                    _coupons.length,
+                    (index) {
+                      final coupon = _coupons[index];
+                      return PromoCard(
+                        discount: coupon['discount']!,
+                        code: coupon['code']!,
+                        onpress: () {
+                          Clipboard.setData(
+                            ClipboardData(text: coupon['code'].toString()),
+                          );
+                          Utils.toastMessage("Code copied to clipboard");
+                        },
+                      );
+                    },
+                  ),
+                ),
         ),
       ),
     );
